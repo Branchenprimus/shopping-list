dd0bc35a1a0ed3887e920863d1bebe1bbf3bc7ab
felix_fla <felixflassak@gmail.com>
2019-10-15 12:01:18 +0200

    Bootstrap CSS CDN, Font Awesome JS, jQuery CDN - Slim version, Popper.JS, Bootstrap JS hinzugefügt. Shoppingcart Favicon. Sidebar, Sidebar Items-Badge , Sidebar Animation, Contentbox mit Items und Suchleiste, Modal (Popupbox) uvm. implementiert.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var selectedListUrl = [];
 
 function POST() {
     var data = JSON.stringify({
-        "name": document.getElementById("myTextarea").value
+        "name": document.getElementById("textarea").value
     });
     var xhr = new XMLHttpRequest();
 
@@ -35,13 +35,14 @@ function GetItems() {
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             var einkaufsliste = JSON.parse(this.responseText)
-            text = ""
+            var text = ""
             for (i = 0; i < Object.keys(einkaufsliste.items).length; i++) {
-                text += '<ul>' + einkaufsliste.items[i].name + ("<button>X</button>" + '</ul>') ;
+                text += '<li class="list-group-item">' + einkaufsliste.items[i].name + ( + '</li>') ;
             }
         }
         text += ""
         document.getElementById('items').innerHTML = text;
+        document.getElementById('quantity').innerHTML= Object.keys(einkaufsliste.items).length + 1;
     });
     xhr.open("GET",
         "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d931300ac8b120017a74aa6");
@@ -62,8 +63,8 @@ function GetListHeader() {
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             var listHeader = JSON.parse(this.responseText)
-            document.getElementById("_id").innerHTML = listHeader._id
-            document.getElementById("userId").innerHTML = listHeader.userId
+          
+            
             document.getElementById("listname").innerHTML = listHeader.name
         }
     });
@@ -78,40 +79,3 @@ function GetListHeader() {
 function deleteItem(){
 
 }
-
-
-
-/*   DELETE Item von Liste */
-
-// function (selectedListUrl, itemId) {
-//     var data = null;
-//     var xhr = new XMLHttpRequest();
-//     xhr.withCredentials = true;
-//     xhr.addEventListener("readystatechange", function () {
-//         if (this.readyState === this.DONE) {
-//             console.log(this.responseText);
-//         }
-//     });
-//     xhr.open("DELETE",
-//         "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + selectedListUrl + "/items5" + itemId
-//     );
-//     xhr.send(data);
-
-
-/* Update Item */
-
-    // var data = JSON.stringify({
-    //     "bought": true
-    // });
-    // var xhr = new XMLHttpRequest();
-    // xhr.withCredentials = true;
-    // xhr.addEventListener("readystatechange", function () {
-    //     if (this.readyState === this.DONE) {
-    //         console.log(this.responseText);
-    //     }
-    // });
-    // xhr.open("PUT",
-    //     "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d931300ac8b120017a74aa6/items/5d964f519ddf3600176d7bbc"
-    // );
-    // xhr.setRequestHeader("content-type", "application/json");
-    // xhr.send(data);
