30a21d07e011d573ba1df4f36850f760a7055b61
Jan Wardenga <jan.wardenga@googlamil.com>
2019-10-26 13:30:01 +0200

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # shopping-list
+
+Ideen:
+-Darkmode
+-Blindmode (text to speech?)
+-Speech-to-text
+-sidebar spezifisch für mobile
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var url = "https://shopping-lists-api.herokuapp.com/api/v1/lists/"
 var listid = "5d931300ac8b120017a74aa6"
 
+
 GetListHeader();
 GetItems();
 
@@ -11,8 +12,9 @@ addEventListener("keydown", function (e) {
     }
 });
 
-
-/*XMLHTTPRequest*/
+/* ---------------------------------------------------
+    POST A NEW ITEM
+----------------------------------------------------- */
 function POST() {
     var data = JSON.stringify({
         "name": document.getElementById("textarea").value
@@ -21,7 +23,6 @@ function POST() {
     var xhr = new XMLHttpRequest();
 
     xhr.open("POST", url + listid + "/items");
-    //"https://shopping-lists-api.herokuapp.com/api/v1/lists/5d931300ac8b120017a74aa6/items"
     xhr.setRequestHeader("content-type", "application/json");
 
     xhr.onload = function () {
@@ -39,7 +40,9 @@ function POST() {
 
 
 
-/*  GET Einkaufslisten Items */
+/* ---------------------------------------------------
+    GET ITEM //Hole alle Items aus der aktuellen Liste
+----------------------------------------------------- */
 function GetItems() {
     var data = null;
     var xhr = new XMLHttpRequest();
@@ -49,8 +52,7 @@ function GetItems() {
             var einkaufsliste = JSON.parse(this.responseText)
             text = ""
             for (i = 0; i < Object.keys(einkaufsliste.items).length; i++) {
-                // text += '<li class="list-group-item">' + einkaufsliste.items[i].name + '<button class="btn btn-outline-secondary" id="' + einkaufsliste.items[i]._id + '"onclick="deleteItem(this.id)"></button></li>';
-                text += ' <li class="list-group-item">' + einkaufsliste.items[i].name + '<span id="btn_items"> <span id="items_bought"> <button class="btn btn-outline-secondary" id="btn_bought"> <i class="fas fa-check"></i> </button> </span> <span id="items_delite"><button class="btn btn-outline-secondary" id="' + einkaufsliste.items[i]._id + '"onclick="deleteItem(this.id)"> <i class="fas fa-trash-alt"></i> </button> </span></span> </li>';
+                text += ' <li class="list-group-item">' + einkaufsliste.items[i].name + '<span id="btn_items"> <span id="items_bought"> <button class="btn btn-outline-secondary" id="' + einkaufsliste.items[i]._id + '"onclick="update(this.id)"> <i class="fas fa-check"></i> </button> </span> <span id="items_delite"><button class="btn btn-outline-secondary" id="' + einkaufsliste.items[i]._id + '"onclick="deleteItem(this.id)"> <i class="fas fa-trash-alt"></i> </button> </span></span> </li>';
                 document.getElementsByTagName("button").id = "newid";
             }
             console.log(Object.keys(einkaufsliste.items).length);
@@ -64,8 +66,9 @@ function GetItems() {
 }
 
 
-
-// /* GET List Header */
+/* ---------------------------------------------------
+    GET LIST HEADER //Hier wird der Listenname abgerufen. Hier muss noch die funktion, alle Listen anzuzeigen implementiert werden
+----------------------------------------------------- */
 
 function GetListHeader() {
 
@@ -85,12 +88,13 @@ function GetListHeader() {
 
     xhr.open("GET", url + listid);
     xhr.setRequestHeader("content-type", "application/json");
-
     xhr.send(data);
 
 }
 
-//delete Item
+/* ---------------------------------------------------
+    DELETE ITEM
+----------------------------------------------------- */
 
 function deleteItem(clicked_id) {
 
@@ -106,36 +110,23 @@ function deleteItem(clicked_id) {
     xhr.send(null);
 }
 
-// Items Löschen
-// function deleteItem(clicked_id) {
-//     var data = JSON.stringify(false);
+/* ---------------------------------------------------
+    UPDATE ITEM
+----------------------------------------------------- */
+
+function update(clicked_id) {
+
+    var data = null;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            var einkaufsliste = JSON.parse(this.responseText)
+            // einkaufsliste.items[0].bought = true
+        }
+    });
 
 
-
-//     var xhr = new XMLHttpRequest();
-//     xhr.withCredentials = true;
-
-//     xhr.addEventListener("readystatechange", function () {
-//         if (this.readyState === this.DONE) {
-//             console.log(this.responseText);
-//         }
-//     });
-
-//     xhr.open("DELETE", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d931300ac8b120017a74aa6/items/5d9e2f5b3197880017e92185");
-//     xhr.setRequestHeader("content-type", "application/json");
-
-
-
-//     // xhr.onload = function () {
-//     //     if (xhr.readyState === xhr.DONE) {
-//     //         if (xhr.status === 200) {
-//     //             console.log(xhr.response);
-//     //             GetItems(); //Funktionsaufruf für die Itemliste
-
-//     //         }
-//     //     }
-//     // };
-//     xhr.send(data);
-//     console.log(xhr.status)
-
-// }
+    xhr.open("PUT", url + listid + "/items/" + clicked_id);
+    xhr.send(data);
+}
